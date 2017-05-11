3c6d77e6db9e94fa17513fc9748c5c4d4725fdec
Marco Esposito <marco.esposito@tum.de>
2017-05-11 16:49:00 +0200

    Added plot with raw NDI data for reference

--- python/applications/algorithm_calibration_ndi.py
+++ python/applications/algorithm_calibration_ndi.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 import h5py
 import numpy as np
+from matplotlib import pyplot as plt
 from pose_denoising import proximal_point_algorithm
 import utils
 
@@ -19,6 +20,26 @@ proximal_point_algorithm(column_poses, smoothed_column_poses,
                          1, 0, 0, 5.0,
                          2, 0.0, 0.0, 100, 0)
 
+
+fig = plt.figure()
+ax1 = fig.add_subplot(121, projection='3d')
+ax2 = fig.add_subplot(122, projection='3d')
+
 smoothed_poses = np.reshape(smoothed_column_poses, (number_of_poses, 4, 4))
 
-utils.plot_transformation_matrix_array(smoothed_poses)
+utils.plot_transformation_matrix_array(sensor_in_world, ax1)
+utils.plot_transformation_matrix_array(smoothed_poses, ax2)
+
+
+def on_move(event):
+    if event.inaxes == ax1:
+        ax2.view_init(elev=ax1.elev, azim=ax1.azim)
+    elif event.inaxes == ax2:
+        ax1.view_init(elev=ax2.elev, azim=ax2.azim)
+    else:
+        return
+    fig.canvas.draw_idle()
+
+c1 = fig.canvas.mpl_connect('motion_notify_event', on_move)
+
+plt.show()
--- python/applications/utils.py
+++ python/applications/utils.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def plot_transformation_matrix_array(ma):
+def plot_transformation_matrix_array(ma, ax=None, fig=None):
 
     translations = ma[:, 3, :3]
     x, y, z = translations[:, 0], translations[:, 1], translations[:, 2]
@@ -21,9 +21,10 @@ def plot_transformation_matrix_array(ma):
     y_axis_comp_norm = np.array([(x/x[-1]) for x in y_axis_comp])
     z_axis_comp_norm = np.array([(x/x[-1]) for x in z_axis_comp])
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    if ax is None:
+        if fig is None:
+            fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
     ax.quiver(x, y, z, x_axis_comp_norm[:, 0], x_axis_comp_norm[:, 1], x_axis_comp_norm[:, 2], normalize=True, color='r')
     ax.quiver(x, y, z, y_axis_comp_norm[:, 0], y_axis_comp_norm[:, 1], y_axis_comp_norm[:, 2], normalize=True, color='b')
     ax.quiver(x, y, z, z_axis_comp_norm[:, 0], z_axis_comp_norm[:, 1], z_axis_comp_norm[:, 2], normalize=True, color='g')
-    plt.show()
