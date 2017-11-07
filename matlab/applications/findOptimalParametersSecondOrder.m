5b5ebe6372783aa4e1b6f814ecb1e9473580021b
Marco Esposito <marco.esposito@tum.de>
2017-11-07 14:21:35 +0100

    Including r in synthetic experiments

--- matlab/applications/findOptimalParametersSecondOrder.m
+++ matlab/applications/findOptimalParametersSecondOrder.m
@@ -31,18 +31,20 @@ for kappa = [10,50,100,500,1000]
     q = RESULTS{count_kappa}(index,3);
     alpha = RESULTS{count_kappa}(index,4);
     
-    % loop over p, q and alpha
+    % loop over r and beta
     count = 1;
-    results = zeros(6,5);
-    for beta = [0, 0.1, 0.5, 1, 2, 5]
-        Pden = denoiseSE3prod(Pnoise,N,1,1,...
-            p,q,0,inner_factor,... % r = 0
-            alpha,beta,0, iteration_steps,inner_steps); % gamma = 0
-        
-        err = comp_dist(Pgt,Pden);
-        results(count,:) = [err, p, q, alpha, beta];
-        count = count + 1;
-        
+    results = zeros(12,6);
+    for r = [1 2]
+        for beta = [0, 0.1, 0.5, 1, 2, 5]
+            Pden = denoiseSE3prod(Pnoise,N,1,1,...
+                p,q,r,inner_factor,... % r = 0
+                alpha,beta,0, iteration_steps,inner_steps); % gamma = 0
+             
+            err = comp_dist(Pgt,Pden);
+            results(count,:) = [err, p, q, r, alpha, beta];
+            count = count + 1;
+            
+        end
     end
 
     RESULTS2{count_kappa} = results;
